808619a83b59767962f5119dc1ac581ae0331824
Semenova-Daria <g.woman.one@gmail.com>
2021-08-19 00:25:20 +0300

    hw-11-colorSwitch

---
+++ colorSwitch.js
@@ -0,0 +1,38 @@
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const refs = {
+    body: document.body,
+    startBtn: document.querySelector('[data-action="start"]'),
+    stopBtn: document.querySelector('[data-action="stop"]'),
+};
+
+refs.startBtn.addEventListener('click', onStartClick);
+refs.stopBtn.addEventListener('click', onStopClick);
+let intervalId = null;
+
+
+
+function onStartClick() {
+    intervalId = setInterval(() => {
+        refs.body.style.background = colors[randomIntegerFromInterval(0, colors.length - 1)]
+    }, 1000);
+    refs.startBtn.disabled = true;
+};
+
+function onStopClick() {
+    clearInterval(intervalId);
+    refs.startBtn.disabled = false;
+}
+
